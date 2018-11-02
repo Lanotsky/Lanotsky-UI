ef00c1cfb4ba1c4a90246e93a64d70b6fac381e9
aaron <aaronlanot@gmail.com>
2018-11-02 10:44:29 +0800

    added app landing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, {Component} from "react";
-import {Container, Sidebar, Content, Logo, LogoContainer} from './App-style';
-import logo from './assets/logo.svg';
-import Modal from './components/widgets/Modal/Modal';
+import './App.scss';
+// import {Container, Sidebar, Content, Logo, LogoContainer} from './App-style';
+// import logo from './assets/logo.svg';
+// import Modal from './components/widgets/Modal/Modal';
 
 const App = () =>{
   return(
-    <Container>
-      <Sidebar>
-          <LogoContainer>
-            <Logo src={logo}/>
-          </LogoContainer>
-      </Sidebar>
-      <Content>
-          <Modal />
-      </Content>
-    </Container>
+    <div className="main-container">
+      <section className="app">
+        
+      </section>
+      <section className="app-dsec">
+      </section>
+    </div>
   )
 }
  
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/scss/main.scss";
+import './assets/imgs/github-404/404-generic.jpg';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
